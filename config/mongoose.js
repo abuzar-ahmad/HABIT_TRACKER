34293c572a2754bdfb4d4cad0097df6934f25651
Abuzar <ahmadabuzar693@gmail.com>
2024-02-08 19:46:58 +0530

    Add files via upload

---
+++ config/mongoose.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+mongoose.connect(`mongodb+srv://shaikhzimran12334:<password>@cluster0.pq1rjyu.mongodb.net/habits_db`);
+// mongoose.connect(`mongodb://127.0.0.1:27017/habits_db`);
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'Error connecting to the db'));
+
+db.once('open', function () {
+  console.log("Successfully connected to the Database");
+});
+
+module.exports = db;
